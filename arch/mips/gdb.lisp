b96e98cd126f864dd29a3fc71deb0533d76816a4
Samium Gromoff <deepfire@elvees.com>
2010-02-26 19:32:12 +0300

    Add EPC to the set of registers exposed to GDB.

--- arch/mips/gdb.lisp
+++ arch/mips/gdb.lisp
@@ -56,7 +56,7 @@
                      (layout space :fpr)
                      (layout space :cop1))
       (export-layout "org.gnu.gdb.mips.cp0" :general nil
-                     (list (layout space :cop0) :badvaddr :status :cause))
+                     (list (layout space :cop0) :badvaddr :status :cause :epc))
       (when (next-method-p)
         (call-next-method)))))
 
@@ -76,8 +76,11 @@
             (write-line (describe-core-memory-region o "ram" (base e) (size e)) str)))))))
 
 (defmethod core-register-order ((o mips-core))
-  (list :r0 :r1 :r2 :r3 :r4 :r5 :r6 :r7 :r8 :r9 :r10 :r11 :r12 :r13 :r14 :r15 :r16 :r17 :r18 :r19 :r20 :r21 :r22 :r23 :r24 :r25 :r26 :r27 :r28 :r29 :r30 :r31
+  (list ;; mandatory part
+        :r0 :r1 :r2 :r3 :r4 :r5 :r6 :r7 :r8 :r9 :r10 :r11 :r12 :r13 :r14 :r15 :r16 :r17 :r18 :r19 :r20 :r21 :r22 :r23 :r24 :r25 :r26 :r27 :r28 :r29 :r30 :r31
         :hi :lo :pc
         :f0 :f1 :f2 :f3 :f4 :f5 :f6 :f7 :f8 :f9 :f10 :f11 :f12 :f13 :f14 :f15 :f16 :f17 :f18 :f19 :f20 :f21 :f22 :f23 :f24 :f25 :f26 :f27 :f28 :f29 :f30 :f31
         :fir :fcsr
-        :badvaddr :status :cause))
+        :badvaddr :status :cause
+        ;; extension
+        :epc))
